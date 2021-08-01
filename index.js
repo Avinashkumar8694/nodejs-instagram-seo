8446c31d18154425ff126f30baea1763b39cafa0
Avinash Kumar Gupta <avinash.gupta@neutrinos.co>
2021-08-01 18:53:32 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-
 const dotenv = require("dotenv");
-const puppeteer = require("puppeteer");
-const instagram = require('./instagram');
+const instagram = require("./instagram");
 dotenv.config();
 const username = process.env.INSTAGRAM_USERNAME;
 const password = process.env.INSTAGRAM_PASSWORD;
 
 (async () => {
-    try{
-        await instagram.initialize(puppeteer);
-        await ig.login(username,password);
-    } catch(error){
-        console.log(error);
-        ig.close();
-    }
-});
+  try {
+    await instagram.initialize();
+    await instagram.login(username, password);
+    await instagram.skipConfirmationWindow();
+    await instagram.collectInstaPost();
+    process.on("exit", instagram.close);
+    process.on("SIGINT", instagram.close);
+  } catch (error) {
+    console.log(error);
+  }
+})();
